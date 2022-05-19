24b17fd3f56f933fbb0942e859ea764a35c9a689
Xu Jiyu <matexu97@gmail.com>
2022-05-19 14:52:59 +0200

    teszt meghívása

--- src/main/TorpedoTeszt.java
+++ src/main/TorpedoTeszt.java
@@ -1,11 +1,15 @@
 package main;
+
 public class TorpedoTeszt {
+
     public static void main(String[] args) {
+        tesztLoves(4);
+    }
+
+    public String tesztLoves(int poz) {
         Hajo hajo = new Hajo();
         String t = hajo.talalat(4);
-        assert t.equals("talált"):"nem jó a találat ellenőrzése";
-    }
-    public String tesztLoves(int poz){
+        assert t.equals("talált") : "nem jó a találat ellenőrzése";
         return "";
     }
 }
